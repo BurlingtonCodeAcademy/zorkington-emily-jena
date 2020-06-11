c3fc24e994a6a87d8954f902b49131a1b46c9b1c
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-06-11 16:54:11 -0400

    error city

--- index.js
+++ index.js
@@ -10,92 +10,107 @@ function ask(questionText) {
 
 //Global Variable Declaration
 //Welcome message
+let transitions;
+let currentRoom; 
+
 let welcomeMessage = "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign."
 
 //Sign message
-let sign= "The sign says 'Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345.'"
+let sign = "The sign says 'Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345.'"
+
+let lookupTable
 
 //Rooms class used to create the game rooms
-class Room{
-  constructor(name, description, inventory){
-    this.name= name;
-    this.description= description;
-    this.inventory= inventory
+class Room {
+  constructor(name, description, inventory) {
+    this.name = name;
+    this.description = description;
+    this.inventory = inventory
+  }
+
+  changeRoom(nextState) {
+    //console.log(currentRoom + nextState)
+    if (transitions[currentRoom.name].canChangeTo.includes(nextState)) {
+      currentRoom = lookupTable[nextState];
+     return currentRoom.description;
+    } else {
+      return `You can not move that way!`;
+    }
   }
 }
+  // Game Rooms
+let mainStreet = new Room("main street", "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign.", ["sign"]);
 
-// Game Rooms
-let mainStreet = new Room("Main Street", "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign.", ["sign"]);
+let foyer = new Room("foyer", "You are in a foyer. Or maybe it's an antechamber. Or a vestibule. Or an entryway. Or an atrium. Or a narthex.But let's forget all that fancy flatlander vocabulary, and just call it a foyer. In Vermont, this is pronounced 'FO-ee-yurr'. A copy of Seven Days lies in a corner.", ["paper"]);
 
-let foyer= new Room ("foyer", "You are in a foyer. Or maybe it's an antechamber. Or a vestibule. Or an entryway. Or an atrium. Or a narthex.But let's forget all that fancy flatlander vocabulary, and just call it a foyer. In Vermont, this is pronounced 'FO-ee-yurr'. A copy of Seven Days lies in a corner.", ["paper"]);
+let classroom = new Room("classroom", "Welcome to the classroom. Say hello to Bob, your excellent instructor.", ["Bob"]);
 
-let classroom = new Room ("classroom", "Welcome to the classroom. Say hello to Bob, your excellent instructor.", ["Bob"]);
+let pizzaShop = new Room("pizza shop", "This is Mr. Mikes. Feel free to buy a slice!", ["pizza"]);
 
-let pizzaShop= new Room ("pizza shop", "This is Mr. Mikes. Feel free to buy a slice!", ["pizza"]);
+let muddys = new Room("muddys'", "Hello! Would you like some tea for Bob? I know how he gets without it!", ["tea"]);
 
-let muddys= new Room ("Muddys'", "Hello! Would you like some tea for Bob? I know how he gets without it!", ["tea"])
+//This function checks whether you can move from one room to another. If you can prints the description of the room
+
+lookupTable = {
+  //map strings to the state options they represent
+  'main street': mainStreet,       
+  'foyer': foyer,
+  'classroom': classroom,
+  'pizza shop': pizzaShop,
+  'muddys': muddys
+}
 
 //State machine controls which room you can go into 
-
-let states = {
-  'Main Street': { canChangeTo: ['foyer', 'pizza shop', 'Muddys'] },
-  'foyer': { canChangeTo: ['classroom', 'Main Street'] },
-  'Mr. Mikes': { canChangeTo: ['Main Street'] },
-  'Muddys': { canChangeTo: ['Main Street'] }
+transitions = {
+  'main street': { canChangeTo: ['foyer', 'pizza shop', 'muddys'] },
+  'foyer': { canChangeTo: ['classroom', 'main street'] },
+  'pizza shop': { canChangeTo: ['main street'] },
+  'muddys': { canChangeTo: ['main street'] }
 };
 
-let currentState = "Main Street";
+currentRoom = mainStreet;
 
-function enterState(newState) {
-  let validTransitions = states[currentState].canChangeTo;
-  if (validTransitions.includes(newState)) {
-    currentState = newState;
-  } else {
-    console.log( 'Invalid state transition attempted - from ' + currentState + ' to ' + newState);
-  }
-}
+currentRoom.changeRoom('muddys')
+console.log(currentRoom)
+currentRoom.changeRoom('main street')
+console.log(currentRoom)
+console.log(currentRoom.changeRoom('foyer'))
+console.log(currentRoom.changeRoom('classroom'))
+console.log(currentRoom.changeRoom('muddys'))
 
-
-// Start function that begins the game. Displays welcome message and goes into the play function. 
-function start(){
-  console.log(welcomeMessage);
-  play();
-}
 
 //Function that allows various inputs 
 async function play() {
   let input = "";
 
-//While the input is not exit, the game awaits input at every turn 
-  while(input !=="exit"){
-    input= await ask('>_ ');
+  //While the input is not exit, the game awaits input at every turn 
+  while (input !== "exit") {
+    input = await ask('>_ ');
 
-//Various actions that the player can take 
-    if (input.includes('examine') || input.includes('read')|| input.includes("read sign")) {
+    //Various actions that the player can take 
+    if (input.includes('examine') || input.includes('read') || input.includes("read sign")) {
       console.log(sign);
 
 
-    } else if (input.includes('take') || input.includes('take note')) {
+    } else if (input.includes('take') || input.includes('take sign')) {
       console.log(`That would be selfish. How will other students find their way?`)
-  
 
-    } else if(input.includes('exit')||input.includes("quit")){
+
+    } else if (input.includes('exit') || input.includes("quit")) {
       console.log("Thank you for playing! Good bye!");
       process.exit();
 
     } else {
       console.log('Sorry. I don\'t how to ' + input)
-   
+
     }
   }
 }
 
-//The functions to begin the game. 
-start();
-play();
+//These functions are called to begin the game. 
+//start();
+//play();
 
-
-//Exits the game
 
 
 
