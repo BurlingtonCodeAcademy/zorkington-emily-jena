a48cb239ca82e88a61e943d9aa0537978295810a
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-06-12 11:23:08 -0400

    paper taking works

--- index.js
+++ index.js
@@ -9,26 +9,36 @@ function ask(questionText) {
 }
 
 //Global Variable Declaration
-//Welcome message
 let transitions;
 let currentRoom;
 let door;
+
 //Welcome message 
-let welcomeMessage = "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign."
+let welcomeMessage = "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign. You can go to muddys, foyer, classroom, pizza shop and main street by typing in 'go to ___'. "
 
 //Sign message
 let sign = "The sign says 'Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345.'"
 
+//Player inventory
+let player ={
+  status: ["full"],
+  inventory: []
+}
+
+//Door object
  door = {
   name: door,
   description: "needs code",
+  //This function unlocks the door with the proper door code. 
   unlock: async function unlock() {
       let code= await ask ("What is your code\n");
       if (code=== "12345"){
       console.log("Success! The door opens. You enter the foyer and the door shuts behind you. ");
       console.log(currentRoom.changeRoom('foyer'));
+      play();
     } else {
-      console.log("Bzzzzt! The door is still locked!")
+      console.log("Bzzzzt! The door is still locked!");
+      play();
     }
   }
   } 
@@ -58,7 +68,7 @@ let sign = "The sign says 'Welcome to Burlington Code Academy! Come on up to the
     // Game Rooms
     let mainStreet = new Room("main street", "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign.", ["sign",]);
 
-    let foyer = new Room("foyer", "You are in a foyer. Or maybe it's an antechamber. Or a vestibule. Or an entryway. Or an atrium. Or a narthex.But let's forget all that fancy flatlander vocabulary, and just call it a foyer. In Vermont, this is pronounced 'FO-ee-yurr'. A copy of Seven Days lies in a corner.", ["paper"]);
+    let foyer = new Room("foyer", "You are in a foyer. Or maybe it's an antechamber. Or a vestibule. Or an entryway. Or an atrium. Or a narthex.But let's forget all that fancy flatlander vocabulary, and just call it a foyer. In Vermont, this is pronounced 'FO-ee-yurr'. A copy of Seven Days lies in a corner, help yourself!", ["paper"]);
 
     let classroom = new Room("classroom", "Welcome to the classroom. Say hello to Bob, your excellent instructor.", ["Bob"]);
 
@@ -107,12 +117,15 @@ let sign = "The sign says 'Welcome to Burlington Code Academy! Come on up to the
         if (input.includes('examine') || input.includes('read') || input.includes("read sign")) {
           console.log(sign);
           
-
-
-        }if (input.includes('enter code') || input.includes("key in") && currentRoom===mainStreet) {
+        }else if (input.includes('enter code') || input.includes("key in") && currentRoom===mainStreet) {
           door.unlock();
 
-        }else if (input.includes('take') || input.includes('take sign')) {
+        } else if(input.includes('take paper')|| input.includes('take seven days')&&currentRoom===foyer){
+          player.inventory.push("paper");
+          console.log ("You pick up the paper and leaf through it looking for comics and ignoring the articles, just like everybody else.");
+          console.log (player.inventory);
+          
+        }else if (input.includes('take') || input.includes('take sign')&& currentRoom===mainStreet) {
           console.log(`That would be selfish. How will other students find their way?`)
 
 
@@ -124,8 +137,12 @@ let sign = "The sign says 'Welcome to Burlington Code Academy! Come on up to the
           console.log(currentRoom.changeRoom('muddys'))
         }
         //Come back to foyer
-        else if (input.includes("go to foyer") || input.includes("foyer")) {
+        else if (input.includes("go to foyer") || input.includes("foyer")&& currentRoom !==mainStreet) {
           console.log(currentRoom.changeRoom('foyer'))
+
+        }else if (input.includes("go to foyer") || input.includes("foyer")&& currentRoom ===mainStreet) {
+           door.unlock();
+
 
         } else if (input.includes("go to classroom") || input.includes("classroom")) {
           console.log(currentRoom.changeRoom('classroom'))
@@ -133,7 +150,7 @@ let sign = "The sign says 'Welcome to Burlington Code Academy! Come on up to the
         } else if (input.includes("go to main street") || input.includes("main street")) {
           console.log(currentRoom.changeRoom('main street'))
 
-        } else if (input.includes("go to pizza shop") || input.includes("mr mikes")) {
+        } else if (input.includes("go to pizza shop") || input.includes("pizza shop")) {
           console.log(currentRoom.changeRoom('pizza shop'))
         }
         else {
