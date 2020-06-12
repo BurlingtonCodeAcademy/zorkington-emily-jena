bb2bf22d4efdcd404e9577c4dabdddc497732e81
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-06-12 13:43:04 -0400

    main street working

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let currentRoom;
 let door;
 
 //Welcome message 
-let welcomeMessage = "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign. You can go to muddys, foyer, classroom, pizza shop and main street by typing in 'go to ___'. "
+let welcomeMessage = "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign. You can go to muddys, foyer, classroom, pizza shop and main street by typing in the name of the place.  "
 
 //Sign message
 let sign = "The sign says 'Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345.'"
@@ -29,11 +29,12 @@ let player = {
 door = {
   name: door,
   description: "needs code",
-  //This function unlocks the door with the proper door code. 
+  //This function unlocks the door with the proper door code. Changes the foyer door so that it is always unlocked.  
   unlock: async function unlock() {
-    let code = await ask("What is your code\n");
+    let code = await ask("What is your code?\n");
     if (code === "12345") {
       console.log("Success! The door opens. You enter the foyer and the door shuts behind you. ");
+      foyer.unlocked= "unlocked";
       console.log(currentRoom.changeRoom('foyer'));
       play();
     } else {
@@ -48,10 +49,11 @@ let lookupTable
 
 //Rooms class used to create the game rooms
 class Room {
-  constructor(name, description, inventory) {
+  constructor(name, description, inventory, unlocked) {
     this.name = name;
     this.description = description;
-    this.inventory = inventory
+    this.inventory = inventory;
+    this.unlocked= unlocked
   }
 
   changeRoom(nextState) {
@@ -64,15 +66,15 @@ class Room {
   }
 }
 // Game Rooms
-let mainStreet = new Room("main street", "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign.", ["sign",]);
+let mainStreet = new Room("main street", "182 Main street. You are standing on Main Street between Church and South Winooski. \n There is a door here. A keypad sits on the handle. On the door is a handwritten sign.", ["sign",], "unlocked");
 
-let foyer = new Room("foyer", "You are in a foyer. Or maybe it's an antechamber. Or a vestibule. Or an entryway. Or an atrium. Or a narthex.But let's forget all that fancy flatlander vocabulary, and just call it a foyer. In Vermont, this is pronounced 'FO-ee-yurr'. A copy of Seven Days lies in a corner, help yourself!", ["a paper"]);
+let foyer = new Room("foyer", "You are in a foyer. Or maybe it's an antechamber. Or a vestibule. Or an entryway. Or an atrium. Or a narthex.But let's forget all that fancy flatlander vocabulary, and just call it a foyer. In Vermont, this is pronounced 'FO-ee-yurr'. A copy of Seven Days lies in a corner, help yourself!", ["a paper"], "locked");
 
-let classroom = new Room("classroom", "Welcome to the classroom. Say hello to Bob, your excellent instructor.", ["Bob"]);
+let classroom = new Room("classroom", "Welcome to the classroom. Say hello to Bob, your excellent instructor.", ["Bob"], "unlocked");
 
-let pizzaShop = new Room("pizza shop", "This is Mr. Mikes. Feel free to buy a slice!", ["pizza"]);
+let pizzaShop = new Room("pizza shop", "This is Mr. Mikes. Feel free to buy a slice!", ["pizza"], "unlocked");
 
-let muddys = new Room("muddys", "Hello! Would you like some tea for Bob? I know how he gets without it!", ["tea"]);
+let muddys = new Room("muddys", "Hello! Would you like some tea for Bob? I know how he gets without it!", ["tea"], "unlocked");
 
 //This function checks whether you can move from one room to another. If you can prints the description of the room
 
@@ -115,9 +117,7 @@ async function play() {
     if (input.includes('examine') || input.includes('read') || input.includes("read sign")) {
       console.log(sign);
 
-    } else if (input.includes("i") || input.includes("inventory") || input.includes("take inventory")) {
-      console.log("You are carrying: " + player.inventory)
-    }
+    } 
 
     else if (input.includes('enter code') || input.includes("key in") && currentRoom === mainStreet) {
       door.unlock();
@@ -138,15 +138,16 @@ async function play() {
     } else if (input.includes('take') || input.includes('take sign') && currentRoom === mainStreet) {
       console.log(`That would be selfish. How will other students find their way?`)
 
-
+//Movement statements
     } else if (input.includes("go to muddys") || input.includes("muddys")) {
       console.log(currentRoom.changeRoom('muddys'))
     }
-    //Come back to foyer
-    else if (input.includes("go to foyer") || input.includes("foyer") && currentRoom !== mainStreet) {
-      console.log(currentRoom.changeRoom('foyer'))
+//If you want to go to foyer, you must enter a code
+    else if (input.includes("foyer") && foyer.unlocked==="unlocked") {
+      console.log(currentRoom.changeRoom('foyer'));
+      
 
-    } else if (input.includes("go to foyer") || input.includes("foyer") && currentRoom === mainStreet) {
+    } else if (input.includes("foyer") && foyer.unlocked==="locked") {
       door.unlock();
 
 
@@ -154,10 +155,15 @@ async function play() {
       console.log(currentRoom.changeRoom('classroom'))
 
     } else if (input.includes("go to main street") || input.includes("main street")) {
+    
       console.log(currentRoom.changeRoom('main street'))
 
     } else if (input.includes("go to pizza shop") || input.includes("pizza shop")) {
       console.log(currentRoom.changeRoom('pizza shop'))
+
+    }else if (input.includes("i") || input.includes("inventory") || input.includes("take inventory")) {
+      console.log("You are carrying: " + player.inventory)
+    
     }
     else {
       console.log('Sorry. I don\'t how to ' + input + " Try again. ")
